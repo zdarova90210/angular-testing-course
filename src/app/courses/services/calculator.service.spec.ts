c696d26bfde647595d345980cf2bec485a5dffad
Maksim Mirbabaev <mmirbabaev@yotateam.com>
2021-07-18 23:34:22 +0300

    first jasmine specs

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,11 +1,18 @@
+import {CalculatorService} from './calculator.service';
+import {LoggerService} from './logger.service';
+
 describe('CalculatorService', () => {
 
   it('should add two numbers', () => {
-    pending();
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.add(2, 2);
+    expect(result).toBe(4);
   });
 
   it('should subtract two numbers', () => {
-    fail();
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.subtract(2, 2);
+    expect(result).toBe(0, 'unexpected subtraction result');
   });
 
 });
